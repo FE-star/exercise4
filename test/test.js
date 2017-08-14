a032bf0a39dbd4e3148b92bc2f51049e6357a2b1
wind4gis <mid_autumn@126.com>
2017-08-14 19:59:20 +0800

    测试通过，原地爆炸

--- test/test.js
+++ test/test.js
@@ -4,10 +4,12 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 箭头函数的this在第一次使用的使用就绑定了作用域为obj，之后就不会被修改，
+          // 相当于setTimeout(()=>{}.bind(this))，在使用时就显式绑定作用域，所以不会出现默认绑定的情况
+          this.should.equal(obj)
           done()
         }, 0)
-      }
+      }      
     }
     obj.say()
   }) 
@@ -15,7 +17,9 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      //由于是在全局环境（调用环境）里使用的test函数，所以这里的this是全局环境
+      //因为test前没有使用任何对象进行修饰或者绑定，所以使用默认绑定规则
+      this.should.equal(global)
     }
     test()
   })
@@ -26,7 +30,9 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            //使用bind显式绑定至obj对象，但是此时obj对象还没有声明等于UNdefined，
+            //此刻应用默认绑定规则，所以this会变成全局对象
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +45,8 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 因为obj已经被定义了，所以此刻绑定至obj变量
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
