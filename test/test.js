606f9b67240ca5f203e59469e2908fbfe4e535c4
iSunlei <qzsunlei@163.com>
2017-08-19 23:10:45 +0800

    My exercise4!

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
           done()
         }, 0)
       }
@@ -15,7 +15,7 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      this.should.equal(global)
     }
     test()
   })
@@ -26,12 +26,12 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            this.should.equal(global)
           }
-          return _say.bind(obj)
+          return _say.bind(obj)   // obj 只做了声明 未事先赋值 所以 obj === undefined
         }()
       }
-      obj.say()
+      obj.say()     // obj.say 先执行 返回 _say.bind(obj)， 再 () 直接调用
     })
 
     it('bind normal', function () {
@@ -39,7 +39,7 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
