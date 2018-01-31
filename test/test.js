2958c773e3cf6ff72b8b952af4210bf219fee0f8
null-br <wanglu1@yy.com>
2018-01-31 10:59:18 +0800

    push exercise4

--- test/test.js
+++ test/test.js
@@ -4,18 +4,20 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
           done()
         }, 0)
       }
     }
+    // setTimeout一般指向window，但是在箭头函数中，不会创建自己的this，使用封闭执行上下文的this值
     obj.say()
   }) 
 
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      // 浏览器环境this指向window，node环境this指向global
+      this.should.equal(global)
     }
     test()
   })
@@ -26,7 +28,9 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            // bind，call改变this的指向，obj由于变量提升，为undefined，_say.bind(obj) === _say.bind(undefined),
+            // 在没有指明this的情况下，this在node环境global，浏览器环境指向window
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +43,8 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 定义了obj对象并调用该对象的say方法，所以bind(obj)指向obj，this指向也就指向obj
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
