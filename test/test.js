88cf7a8c1ae9e977a9cdb7c4abb1721de6426fa1
PennyOrAmy <penny_kay@126.com>
2018-02-01 22:22:36 +0800

    fix error

--- test/test.js
+++ test/test.js
@@ -3,19 +3,19 @@ describe('this', function () {
     var obj = {
       say: function () {
         setTimeout(() => {
-          // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 箭头函数没有自己的 this，它的 this 是继承而来；默认指向在定义它时所处的对象（宿主对象），而不是执行时的对象
+          this.should.equal(obj)
           done()
         }, 0)
       }
     }
     obj.say()
-  }) 
+  })
 
   it('global', function () {
+    // 没有直接调用者的函数：严格模式下，this 为 undefined；非严格模式下，this为 window 或 global
     function test() {
-      // this 是什么？想想为什么？
-      this.should.equal(null)
+      this.should.equal(global)
     }
     test()
   })
@@ -25,8 +25,7 @@ describe('this', function () {
       var obj = {
         say: function () {
           function _say() {
-            // this 是什么？想想为什么？
-            this.should.equal(null)
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -38,8 +37,7 @@ describe('this', function () {
       var obj = {}
       obj.say = function () {
         function _say() {
-          // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
