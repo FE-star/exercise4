8844a9829a456c1c72ea503a6e76b6150a87f09d
王蕾 <wanglei@wangleideMBP.lan>
2018-02-03 21:32:09 +0800

    update test.js

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
+// this 指向函数运行时的当前对象
 describe('this', function () {
   it('setTimeout', function (done) {
     var obj = {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
           done()
         }, 0)
       }
@@ -15,7 +16,7 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      this.should.equal(global)
     }
     test()
   })
@@ -26,7 +27,7 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +40,7 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
