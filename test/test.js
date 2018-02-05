3bb79d13b41669b421bc2b2d8bec5d23172f0df7
wancheng7 <525918717@qq.com>
2018-02-05 11:23:31 +0800

    understand

--- test/test.js
+++ test/test.js
@@ -15,7 +15,7 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(global);  //直接在全局环境下调用,为什么不是window？？
+      this.should.equal(global);  //直接在全局环境下调用,为什么不是window？因为在node环境下
     }
     test()
   })
@@ -26,9 +26,9 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(global);  //this指向的是obj的this也就是window
+            this.should.equal(global);  
           }
-          return _say.bind(obj)
+          return _say.bind(obj)   //这里obj传入之前是等于undefined的，因为obj这个对象还只被初始化了，也就是等于undefined，还没有赋值完成，所以传入undefined之后_say中的this就是global
         }()
       }
       obj.say()
@@ -39,9 +39,9 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(obj);  //这个不知道为什么
+          this.should.equal(obj);  //这个不知道为什么,这里的obj?
         }
-        return _say.bind(obj)
+        return _say.bind(obj)   // 这里obj已经被初始化了，也赋值为一个空对象了，所以传入的obj就是一个空对象，_say中的this就是obj
       }()
       obj.say()
     })
