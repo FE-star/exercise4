5d9f888ab53043a618a002bd9e69a2a85900a029
Mcyfeng <35359402+Mcyfeng@users.noreply.github.com>
2018-02-09 12:21:45 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,22 +3,23 @@ describe('this', function () {
     var obj = {
       say: function () {
         setTimeout(() => {
-          // this 是什么？想想为什么？
-          this.should.equal(null)
+          // this 是什么？想想为什么？ this指向obj,因为箭头函数函数
+          this.should.equal(obj);
           done()
         }, 0)
       }
-    }
+    };
     obj.say()
-  }) 
+  }) ;
 
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+
+      this.should.equal(this, global);//没有明确指明时，this指向global
     }
     test()
-  })
+  });
 
   describe('bind', function () {
     it('bind undefined', function () {
@@ -26,24 +27,25 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+              this.should.equal(this,global)
+            // this.should.equal(null)//断言之前还没有执行return方法,所以这里指向的是global
           }
           return _say.bind(obj)
         }()
-      }
+      };
       obj.say()
-    })
+    });
 
     it('bind normal', function () {
-      var obj = {}
+      var obj = {};
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(this,obj)//因为运行的时候已经声明是指向obj
         }
         return _say.bind(obj)
-      }()
+      }();
       obj.say()
     })
   })
-})
+});
