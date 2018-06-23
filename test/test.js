25879109c82b2bfe175ea00a37bfb89b7ae53b4f
guoekzhibu <850997448@qq.com>
2018-06-24 03:12:32 +0800

    exercise4 作业提交

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
+var should = require('should');
 describe('this', function () {
   it('setTimeout', function (done) {
     var obj = {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 箭头函数中的this 指向上一层, 而 obj.say()中say中的this指向调用它的obj
+          this.should.equal(obj)
           done()
         }, 0)
       }
@@ -15,7 +17,8 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      // test() 中的this 指向全局
+      this.should.equal(global)
     }
     test()
   })
@@ -26,7 +29,10 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            // 引擎先生成对象再把对象内存赋值给obj
+            // say 对应的function先保存到内存，再把地址赋值给say。
+            // 而此时obj未初始化,所以 _say.bind(undefined)
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +45,8 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          //this 指向obj , _say改变函数体内this指向是 obj已经初始化 
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
