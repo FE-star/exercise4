723e2eabdd2b63b46bd493fb2f52e6a427d96f58
张泽 <ze.zh@hotmail.com>
2018-06-24 13:46:57 +0800

    shiji-answer

--- test/test.js
+++ test/test.js
@@ -4,7 +4,9 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 如果此处不使用箭头函数的形式，this应当指向的是global，因为异步执行的代码会被认为是在全局环境内执行的
+          // 但此处使用了箭头函数，因为箭头函数没有this，会从自己作用域链的上一层继承this，而上一级是say()的函数作用域，作为obj的方法调用，this指向obj
+          this.should.equal(obj)
           done()
         }, 0)
       }
@@ -15,7 +17,8 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      // test方法是个全局方法，全局方法执行是在全局作用域内，指向global（或者可以认为是global.test()）
+      this.should.equal(global)
     }
     test()
   })
@@ -26,7 +29,7 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +42,7 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
