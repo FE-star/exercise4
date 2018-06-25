d7369d76f1bbb97503f8a3f20940a5bdc852a91b
fridolph <249121486@qq.com>
2018-06-25 09:29:49 +0800

    exercise 4 test passed

--- test/test.js
+++ test/test.js
@@ -4,18 +4,20 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 在箭头函数中，this与封闭词法上下文的this保持一致。在这里是say所在的作用域 obj对象
+          this.should.equal(obj)
           done()
         }, 0)
       }
     }
     obj.say()
-  }) 
+  })
 
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      // 因为下面的代码不在严格模式下，且 this 的值不是由该调用设置的，所以 this 的值默认指向全局对象。
+      this.should.equal(global)
     }
     test()
   })
@@ -26,7 +28,9 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            // 这里相当于简单函数调用，obj.say是一个IIFE，其中并没有obj变量，相当于 bind 了 undefined
+            // 所以this指向全局对象
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +43,8 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // bind 了一个普通对象，this指向该对象
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
