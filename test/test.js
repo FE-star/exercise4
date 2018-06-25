2a0356f614e62ef2b1c550d6bc28983c9a5d33db
snowfighting <songxj0130@126.com>
2018-06-25 11:19:52 +0800

    feat(): exercise4

--- test/test.js
+++ test/test.js
@@ -4,18 +4,27 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 箭头函数的作用域指向它的父级作用域 等价于：
+          // say: function() {
+          //   const _this = this;
+          //   setTimeout(() => {
+          //     _this.should.equal(obj);
+          //     done();
+          //   })
+          // }
+          this.should.equal(obj)
           done()
         }, 0)
       }
     }
     obj.say()
-  }) 
+  })
 
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      // this直接在全局作用域运行
+      this.should.equal(global)
     }
     test()
   })
@@ -26,7 +35,9 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            // say是一个立即执行函数，当执行到 return _say.bind(obj)的时候，obj还没创建完成，实际上传进去的是undefined，所以会被替换为全局对象
+            // node中全局对象指向global
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +50,8 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // say是一个立即执行函数，当执行到 return _say.bind(obj)的时候，obj已经创建完成，所以传进去的就是obj对象
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
