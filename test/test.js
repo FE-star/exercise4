e286401e3bd1b6420043a8f816202b9d0812c910
Sofiya <sofiya.qiaoxu@ksher.net>
2018-11-08 17:15:08 +0800

    this的理解

--- test/test.js
+++ test/test.js
@@ -4,7 +4,8 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          //使用ES6中的箭头函数，此时this会默认绑定在当前函数的词法作用于中。
+          this.should.equal(obj)
           done()
         }, 0)
       }
@@ -15,7 +16,9 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      //默认绑定——非严格模式下，指向window——浏览器中
+      //在node环境下，全局环境下，this指向global
+      this.should.equal(global)
     }
     test()
   })
@@ -26,7 +29,9 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            //js的赋值运算顺序永远都是从右往左的，_say.bind(obj)的时候，obj还没有创建，
+              // 所以为undefined，当bind显示绑定传的为undefined，则表示this指向全局
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +44,8 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+            //强制绑定上了obj
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
