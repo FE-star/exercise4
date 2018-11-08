b96c3ddc3a3ee00de11d7f34aac51ae19f749029
毕道振 <bestzane87@163.com>
2018-11-08 11:19:13 +0800

    增加注释

--- test/test.js
+++ test/test.js
@@ -2,8 +2,9 @@ describe('this', function () {
   it('setTimeout', function (done) {
     var obj = {
       say: function () {
+        // 箭头函数将this绑定为当前执行环境，当前执行上下文为obj
         setTimeout(() => {
-          // this 是什么？想想为什么？
+          // this 是什么？想想为什么?
           this.should.equal(obj)
           done()
         }, 0)
@@ -17,6 +18,7 @@ describe('this', function () {
       // this 是什么？想想为什么？
       this.should.equal(global)
     }
+    // 当前函数的执行环境在全局作用域中，所以this指向的是global
     test()
   })
 
@@ -26,6 +28,8 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
+            // 由于赋值操作是从右向左进行，所以bind操作时obj并未声明，
+            // 所以为undefined，此时将全局作用域传入执行环境参数中
             this.should.equal(global)
           }
           return _say.bind(obj)
@@ -36,9 +40,11 @@ describe('this', function () {
 
     it('bind normal', function () {
       var obj = {}
+      
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
+          // 普通的bind操作，当前函数的执行上下文为绑定的对象
           this.should.equal(obj)
         }
         return _say.bind(obj)
