485da511b5b1b0d5e52f9be5ff54895d1c479f80
zhaopeng <719197352@qq.com>
2018-11-08 21:05:57 +0800

    work

--- test/test.js
+++ test/test.js
@@ -4,7 +4,9 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 由于是在obj对象上调用的函数，调用位置会使用obj上下文来引用function
+          // 而箭头函数会绑定当前函数的上下文
+          this.should.equal(obj)
           done()
         }, 0)
       }
@@ -15,7 +17,9 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      // 此时使用的时默认绑定在非严格模式下为全局对象
+      // node中的全局对象为global
+      this.should.equal(global)
     }
     test()
   })
@@ -26,7 +30,10 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            // say引用的是立即执行函数的返回值
+            // 在执行函数时 obj对象还没有被赋值 所以obj为undefined
+            // bind undefined 时在非严格模式下为全局对象
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +46,8 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 立即执行函数执行时obj已经定义 正常将obj对象作为_say的上下文
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
