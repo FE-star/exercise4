7105ef9b4348d1cfba3650cb00f7f1b742e813e5
1269894710 <1269894710@qq.com>
2018-11-09 22:48:17 +0800

    修改测试

--- test/test.js
+++ test/test.js
@@ -4,7 +4,8 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 本身setTimeout如果不制定作用域的话，内部应该是指向全局的，但是setTimeout在声明函数时使用了=>箭头函数，由于箭头函数不绑定this，而是与声明时所在的父作用域。所以这里指向的就是obj本身
+          this.should.equal(obj)
           done()
         }, 0)
       }
@@ -15,7 +16,8 @@ describe('this', function () {
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      // 全局调用函数，肯定指向全局，这里nodejs的全局是global，如果是浏览器的话，那么应该指向的是window(严格模式下是undefined)
+      this.should.equal(global)
     }
     test()
   })
@@ -26,7 +28,8 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            // func.bind(context)函数会修改函数内部的this指向为context，使得函数不论在何时调用，其内部都指向context，但是这里给obj的属性say赋值时，obj还没有完全创建完毕，没有分配相应的地址，所以在内部调用obj的时候还是undefined，而bind函数如果传递undefined相当于不传，指向全局global
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +42,8 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          // 先创建了一个空的obj对象。然后调用bind函数传入，所以调用时指向obj
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
