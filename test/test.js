d0dcbb0201c92d81648519b560691a9f383cc1aa
chenyu <iambigchen@163.com>
2018-11-11 14:39:17 +0800

    联系完成

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ describe('this', function () {
     var obj = {
       say: function () {
         setTimeout(() => {
-          // this 是什么？想想为什么？
-          this.should.equal(null)
+          // this 是什么？想想为什么？ // obj
+          this.should.equal(obj)
           done()
         }, 0)
       }
@@ -14,8 +14,8 @@ describe('this', function () {
 
   it('global', function () {
     function test() {
-      // this 是什么？想想为什么？
-      this.should.equal(null)
+      // this 是什么？想想为什么？ 
+      this.should.equal(global)
     }
     test()
   })
@@ -26,7 +26,7 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            this.should.equal(global)
           }
           return _say.bind(obj)
         }()
@@ -39,7 +39,7 @@ describe('this', function () {
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
         }
         return _say.bind(obj)
       }()
