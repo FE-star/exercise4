d63fd5e5d64c779f4ec5bf8a811aa615b8e78086
louly <louly@rd.netease.com>
2019-08-04 12:30:47 +0800

    lly: preview

--- test/test.js
+++ test/test.js
@@ -4,21 +4,21 @@ describe('this', function () {
       say: function () {
         setTimeout(() => {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj);
           done()
         }, 0)
       }
-    }
+    };
     obj.say()
-  }) 
+  });
 
   it('global', function () {
     function test() {
       // this 是什么？想想为什么？
-      this.should.equal(null)
+      this.should.equal(global)
     }
     test()
-  })
+  });
 
   describe('bind', function () {
     it('bind undefined', function () {
@@ -26,24 +26,25 @@ describe('this', function () {
         say: function () {
           function _say() {
             // this 是什么？想想为什么？
-            this.should.equal(null)
+            this.should.equal(global)
           }
+          //  obj get undefined
           return _say.bind(obj)
         }()
-      }
+      };
       obj.say()
-    })
+    });
 
     it('bind normal', function () {
-      var obj = {}
+      var obj = {};
       obj.say = function () {
         function _say() {
           // this 是什么？想想为什么？
-          this.should.equal(null)
+          this.should.equal(obj)
         }
         return _say.bind(obj)
-      }()
+      }();
       obj.say()
     })
   })
-})
+});
